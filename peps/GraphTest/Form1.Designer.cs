22a5816c506a5abc6bd8bc93948c5612c1e8c969
pizzix <yannick.luzy@gmail.com>
2014-02-18 21:33:32 +0100

    adding graph

--- peps/GraphTest/Form1.Designer.cs
+++ peps/GraphTest/Form1.Designer.cs
@@ -48,7 +48,7 @@
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(853, 461);
+            this.chart1.Size = new System.Drawing.Size(1298, 683);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
@@ -56,7 +56,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(877, 485);
+            this.ClientSize = new System.Drawing.Size(1322, 707);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
--- peps/GraphTest/Form1.cs
+++ peps/GraphTest/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace GraphTest
 {
@@ -14,6 +15,44 @@ namespace GraphTest
         public Form1()
         {
             InitializeComponent();
+
+            List<double> lst_price = new List<double>();
+            lst_price = LectureData(@"../../../DATA/prix.txt");
+            chart1.Series.Clear();
+            chart1.Series.Add("Price");
+            chart1.Series.Add("1.23");
+           // chart1.Series["Price"].AxisLabel = "Price";
+            chart1.Series["Price"].ChartType = SeriesChartType.Spline;
+            chart1.Series["1.23"].ChartType = SeriesChartType.Spline;
+            for(int i = 0; i < lst_price.Count; i++) {
+                chart1.Series["Price"].Points.AddXY(i, lst_price[i]);
+                chart1.Series["1.23"].Points.AddXY(i, 1.23);
+            }
+            
+
+
+        }
+
+        private List<double> LectureData(string fichier) {
+            List<double> lst_double = new List<double>();
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader(fichier);
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                line = line.Replace(".", ",");
+                lst_double.Add(Convert.ToDouble(line));
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.
+            System.Console.ReadLine();
+            return lst_double;
         }
     }
 }
